6022fea35548a7153e212ec53a74de6c5e16690d
sajjad-ali97 <abdabdali2010@gmail.com>
2026-02-14 21:09:20 +0300

    modify projects table

--- database/migrations/2026_02_14_142147_create_projects_table.php
+++ database/migrations/2026_02_14_142147_create_projects_table.php
@@ -5,22 +5,26 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
+
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
+
             $table->id();
 
+            // ===== Basic Project Info =====
             $table->string('title', 150)->nullable();        // اسم المشروع
             $table->string('governorate', 80)->nullable();   // المحافظة
             $table->string('road_name', 150)->nullable();    // اسم الطريق
 
-            $table->date('construction_date')->nullable();   // تاريخ إنشاء الطريق/المشروع
+            // ===== Maintenance Date Only =====
             $table->date('maintenance_date')->nullable();    // تاريخ الصيانة
 
             $table->timestamps();
 
-            $table->index(['governorate']);
-            $table->index(['maintenance_date']);
+            // Indexes
+            $table->index('governorate');
+            $table->index('maintenance_date');
         });
     }
 
